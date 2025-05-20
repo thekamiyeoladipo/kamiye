d8d52d016def1485894522c36c5ac734101e804d
kamiye <kamiye31@gmail.com>
2025-05-20 20:41:25 +0100

    barberJs addition

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,3 +10,24 @@
       close.classList.toggle('hidden');
     });
 
+barba.init({
+  transitions: [
+    {
+      name: 'simple-fade',
+      async leave(data) {
+        // Add fade-out animation to old container
+        data.current.container.classList.add('fade-out');
+        await delay(500); // match the CSS animation duration
+      },
+      enter(data) {
+        // Add fade-in animation to new container
+        data.next.container.classList.add('fade-in');
+      }
+    }
+  ]
+});
+
+// Helper delay function
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
