fbb9f75519349cb81cb52b51b69bc4ffcd7cbd96
kamiye <kamiye31@gmail.com>
2025-05-21 15:15:07 +0100

    back to top button
    fixed bug with button

--- dist/js/main.js
+++ dist/js/main.js
@@ -50,20 +50,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //back to top button
 
-  const backToTopBtn = document.getElementById("backToTop");
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-      backToTopBtn.classList.remove("opacity-0", "pointer-events-none");
+  const backToTopBtn = document.getElementById('backToTop');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+      backToTopBtn.classList.remove('opacity-0', 'pointer-events-none');
+      backToTopBtn.classList.add('opacity-100');
     } else {
-      backToTopBtn.classList.add("opacity-0", "pointer-events-none");
+      backToTopBtn.classList.add('opacity-0', 'pointer-events-none');
+      backToTopBtn.classList.remove('opacity-100');
     }
   });
 
-  backToTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+  backToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   });
 
 
